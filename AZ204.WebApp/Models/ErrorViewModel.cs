cf774c9c5886e407e38090bc27f41e4f434441e7
Administrator <Administrator@FBD76A0AF57851D>
2023-11-02 20:13:25 +0530

    Add project files.

---
+++ AZ204.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AZ204.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
